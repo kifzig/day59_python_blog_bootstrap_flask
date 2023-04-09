8d0a56e0e5c136ba137d5c8ca128b72f03b48529
Kif_Francis <kif.zig@gmail.com>
2023-04-09 13:36:07 -0500

    Add post functionality and post link in main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+import requests
+
+posts = requests.get("https://api.npoint.io/a4fb6e51d351014d0521").json()
+print(posts)
+
 app = Flask(__name__)
 
 @app.route('/')
 def get_all_posts():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=posts)
 
 @app.route('/about')
 def about():
@@ -12,6 +17,15 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
